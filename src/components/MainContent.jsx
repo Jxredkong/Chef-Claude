3fa513da5cfff0a076738152e7505f3530896968
Jxredkong <jaredlovesbutter@gmail.com>
2025-05-20 17:26:30 +0800

    Form handler

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,14 +1,31 @@
+import {useState} from "react"
+
 export default function MainContent(){
+
+    const [ingredients, setIngredients] = React.useState([])
+
+    const ingredientList = ingredients.map((ingredient, index) => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
+
+    function handleSubmit(formData){
+        const ingredient = formData.get("ingredient")
+        setIngredients(prevIngredients => [...prevIngredients, ingredient])
+    }
     return(
         <main>
-            <form className="ingredient-form">
+            <form action={handleSubmit} className="ingredient-form">
                 <input
                     type="text"
-                    placeholder="e.g. eggs"
+                    placeholder="e.g. oregano"
                     aria-label="Add ingredient"
+                    name="ingredient"
                 />
                 <button>Add ingredient</button>
             </form>
+            <ul>
+                {ingredientList}
+            </ul>
         </main>
     )
 }
